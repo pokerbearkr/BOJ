c7cb07acaf502d0b5f4467a3090bc42b45e078c5
pokerbearkr <ogdongwon@gmail.com>
2025-02-25 16:09:59 +0900

    tem

---
+++ src/BOJ/BOJ5567.java
@@ -0,0 +1,30 @@
+package BOJ;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BOJ5567 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        Map<Integer,int[]> map = new HashMap<>();
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(map.containsKey(a)){
+                map.put(a,new int[]{})
+            }
+        }
+
+
+    }
+}
