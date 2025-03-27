02627463044868f1dd1864453df790cf39299a63
pokerbearkr <ogdongwon@gmail.com>
2025-03-28 00:15:57 +0900

    2025.03.28 desktop

---
+++ src/BOJ/BOJ9656.java
@@ -0,0 +1,17 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ9656 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if (N % 2 == 0) {
+            System.out.println("SK");
+        } else {
+            System.out.println("CY");
+        }
+    }
+}
